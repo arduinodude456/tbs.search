d3739e8adda10398c42e631abea330cadc8cbc7c
tbs <onlyformusic456@outlook.de>
2026-09-05 18:47:01 +0200

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -16,7 +16,7 @@ from bs4 import BeautifulSoup
 # ============================================================
 
 START_URLS = [
-    "https://www.youtube.com/"
+    "https://www.martinkaessler.com/50-meistgeschaute-youtube-videos/"
 ]
 
 # Maximale Anzahl NEUER/AKTUALISIERTER Seiten pro Lauf
