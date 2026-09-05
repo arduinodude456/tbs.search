9c1c469e98c4f7e11dec8ae51b08a758f696ca73
tbs <onlyformusic456@outlook.de>
2026-09-05 17:49:51 +0200

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -16,7 +16,7 @@ from bs4 import BeautifulSoup
 # ============================================================
 
 START_URLS = [
-    "https://de.wikipedia.org/wiki/Wikipedia:Hauptseite"
+    "https://archive.org/"
 ]
 
 # Maximale Anzahl NEUER/AKTUALISIERTER Seiten pro Lauf
